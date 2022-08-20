17f130aa114aa89fe449c97bcde791a5bae883b8
4everfree <32147862+4everfree@users.noreply.github.com>
2022-08-20 22:27:19 +0400

    changed selection of driver

--- src/main/test/common/CommonAction.java
+++ src/main/test/common/CommonAction.java
@@ -8,7 +8,7 @@ import org.testng.Assert;
 import java.time.Duration;
 
 import static common.Config.IMPLICIT_WAIT;
-import static common.Config.PLATFORM_AND_BROWSER;
+import static common.Config.BROWSER;
 
 public class CommonAction {
     private static WebDriver driver = null;
@@ -18,12 +18,12 @@ public class CommonAction {
 
     public static WebDriver createDriver() {
         if (driver == null) {
-            switch (PLATFORM_AND_BROWSER) {
-                case "win_chrome":
+            switch (BROWSER) {
+                case "chrome":
                     driver = WebDriverManager.getInstance(DriverManagerType.CHROME).create();
                     break;
                 default:
-                    Assert.fail("Not valid web driver setting in config: " + PLATFORM_AND_BROWSER);
+                    Assert.fail("Not valid web driver setting in config: " + BROWSER);
             }
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
